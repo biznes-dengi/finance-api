f14f8806e0e5ec2fb0607a7619ec3420329dc4c4
Mikita Yankouski <nikitayankouski@gmail.com>
2024-09-11 19:18:52 +0200

    add currency field in SavingViewResponse

--- src/main/java/com/maksyank/finance/saving/boundary/response/SavingViewResponse.java
+++ src/main/java/com/maksyank/finance/saving/boundary/response/SavingViewResponse.java
@@ -1,11 +1,14 @@
 package com.maksyank.finance.saving.boundary.response;
 
+import com.maksyank.finance.saving.domain.enums.CurrencyCode;
+
 import java.math.BigDecimal;
 
 public record SavingViewResponse(
         int id,
         String name,
         BigDecimal balance,
+        CurrencyCode currency,
         BigDecimal targetAmount,
         String image
 ) { }
