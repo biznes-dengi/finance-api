31ead22ec4e0c8fcb4297f50d9548cbdcadd5254
Mikita Yankouski <nikitayankouski@gmail.com>
2024-10-20 10:29:49 +0200

    upd. 5, renaming

--- src/main/java/com/maksyank/finance/saving/boundary/response/SavingAllResponse.java
+++ src/main/java/com/maksyank/finance/saving/boundary/response/SavingAllResponse.java
@@ -3,6 +3,6 @@ package com.maksyank.finance.saving.boundary.response;
 import java.util.List;
 
 public record SavingAllResponse(
-        List<SavingViewResponse> data,
+        List<SavingViewResponse> items,
         boolean hasNext
 ) {}
--- src/main/java/com/maksyank/finance/saving/boundary/response/TransactionAllResponse.java
+++ src/main/java/com/maksyank/finance/saving/boundary/response/TransactionAllResponse.java
@@ -1,11 +1,8 @@
 package com.maksyank.finance.saving.boundary.response;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 import java.util.List;
 
 public record TransactionAllResponse(
-        @JsonProperty("transactions")
-        List<TransactionViewResponse> transactionViewResponseList,
+        List<TransactionViewResponse> items,
         boolean hasNext
 ) {}
