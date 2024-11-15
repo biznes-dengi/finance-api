0b5cdfbe63846628477016b37e891f97bfcedaf9
Mikita Yankouski <nikitayankouski@gmail.com>
2024-11-15 18:59:18 +0100

    exclude : UserDetailsServiceAutoConfiguration

--- runner/src/main/java/com/finance/app/FinanceApplication.java
+++ runner/src/main/java/com/finance/app/FinanceApplication.java
@@ -2,8 +2,9 @@ package com.finance.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
 public class FinanceApplication {
 
     public static void main(String[] args) {
